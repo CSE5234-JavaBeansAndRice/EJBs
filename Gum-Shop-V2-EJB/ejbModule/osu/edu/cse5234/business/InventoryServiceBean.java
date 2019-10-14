e0a9f75917e32a1301b251e92bd8863df176c1a3
Simon John Turner <turner.1418@osu.edu>
2019-10-14 12:15:39 -0400

    MVP of inventory service bean

--- Gum-Shop-V2-EJB/ejbModule/osu/edu/cse5234/business/InventoryServiceBean.java
+++ Gum-Shop-V2-EJB/ejbModule/osu/edu/cse5234/business/InventoryServiceBean.java
@@ -1,8 +1,14 @@
 package osu.edu.cse5234.business;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
+
+import osu.edu.cse5234.business.view.Inventory;
 import osu.edu.cse5234.business.view.InventoryService;
+import osu.edu.cse5234.business.view.Item;
 
 /**
  * Session Bean implementation class InventoryServiceBean
@@ -12,10 +18,37 @@ import osu.edu.cse5234.business.view.InventoryService;
 public class InventoryServiceBean implements InventoryService {
 
     /**
-     * Default constructor. 
+     * Default constructor.
      */
     public InventoryServiceBean() {
         // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public Inventory getAvailableInventory() {
+        Inventory inventory = new Inventory();
+        String[] gums = { "Bubble", "Spearmint", "Peppermint", "Wintergreen",
+                "Cinnamon" };
+        List<Item> items = new ArrayList<Item>();
+        for (int i = 0; i < 5; i++) {
+            Item item = new Item();
+            item.setName(gums[i]);
+            item.setPrice(i + .5);
+            item.setQuantity((i / 3) + 1);
+            items.add(item);
+        }
+        inventory.setItems(items);
+        return inventory;
+    }
+
+    @Override
+    public boolean validateQuantity(List<Item> items) {
+        return true;
+    }
+
+    @Override
+    public boolean updateInventory(List<Item> items) {
+        return true;
     }
 
 }
