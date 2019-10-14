1df40a1f6fd741042a26c6b6eacf51a6d1de7c80
Simon John Turner <turner.1418@osu.edu>
2019-10-14 11:58:39 -0400

    added Item and Inventory

---
+++ Gum-Shop-V2-EJBClient/ejbModule/osu/edu/cse5234/business/view/Inventory.java
@@ -0,0 +1,16 @@
+package osu.edu.cse5234.business.view;
+
+import java.util.List;
+
+public class Inventory {
+    private List<Item> items;
+
+    public List<Item> getItems() {
+        return this.items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+}
---
+++ Gum-Shop-V2-EJBClient/ejbModule/osu/edu/cse5234/business/view/Item.java
@@ -0,0 +1,31 @@
+package osu.edu.cse5234.business.view;
+
+public class Item {
+    private String name;
+    private Double price;
+    private Integer quantity;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Double getPrice() {
+        return this.price;
+    }
+
+    public Integer getQuantity() {
+        return this.quantity;
+    }
+}
